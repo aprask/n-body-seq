9e128497a962daa75d97a6c06736305c2277cb51
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-07 16:09:35 -0500

    Created function for force

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-#define G  6.674(10e-11)
+#define G  6.674*pow(10,-11);
 #define ARGS 5 // num of particles, time step size, num of iterations, how often to dump state
 
+double calculateForce(struct particleNode* nodeA, struct particleNode* nodeB);
 struct particleNode {
     double mass;
     double position;
@@ -16,6 +17,13 @@ struct particleNode {
 };
 
 int main (int argc, char* argv[]) {
-    
+
     return 0;
+}
+
+double calculateForce(struct particleNode* nodeA, struct particleNode* nodeB) {
+    double totalMass = nodeA->mass * nodeB->mass; // distance between particles in space
+    double distanceBetweenParticles = nodeB->position - nodeA->position; // r
+    distanceBetweenParticles = pow(distanceBetweenParticles, 2); // r squared
+    return G(totalMass/distanceBetweenParticles);
 }
