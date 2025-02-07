182659aedc8ed986d7d2f08f024bb95905cb353f
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-07 16:02:13 -0500

    This should be a doubly linked list

--- main.cpp
+++ main.cpp
@@ -12,24 +12,10 @@ struct particleNode {
     double velocity;
     double force;
     struct particleNode* nextParticleInSeq;
+    struct particleNode* previousParticleInSeq;
 };
 
 int main (int argc, char* argv[]) {
-    struct particleNode* testParticle1 = (struct particleNode*)malloc(sizeof(struct particleNode));
-    testParticle1->velocity = 500.25;
-    struct particleNode* testParticle2 = (struct particleNode*)malloc(sizeof(struct particleNode));
-    testParticle2->velocity = 8900.25;
-    struct particleNode* testParticle3 = (struct particleNode*)malloc(sizeof(struct particleNode));
-    testParticle3->velocity = 23323.25;
-
-    testParticle1->nextParticleInSeq = testParticle2;
-    testParticle2->nextParticleInSeq = testParticle3;
-
-    cout << "particle1's next particle's velocity: " << testParticle1->nextParticleInSeq->velocity << endl;;
-    cout << "particle1's next next particle's velocity: " << testParticle1->nextParticleInSeq->nextParticleInSeq->velocity << endl;
-
-    free(testParticle1);
-    free(testParticle2);
-    free(testParticle3);
+    
     return 0;
 }
