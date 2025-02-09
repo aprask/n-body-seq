6d06d92f90021aab074034bc1ccc5c12238e1938
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-08 19:28:17 -0500

    Cleaned functions -- I understand physics now :)

--- main.cpp
+++ main.cpp
@@ -12,7 +12,10 @@ const double G = 6.674*pow(10,-11); // G for grav force
 const double SOFTENING_FACTOR = 0.000000001; // this is just an arbitary float (I wasn't sure what "softening factor" actuall was...)
 
 double calculateForce(double mass1, double coordinate1, double mass2, double coordinate2);
-double calculateAcceleration(struct particleNode* p, double force);
+double calculateAcceleration(double force, double mass);
+double calculatePosition(double coordinate, double velocity, double delta_t);
+double calculateVelocity(double velocity, double acceleration, double delta_t);
+
 struct position {
     double x;
     double y;
@@ -32,7 +35,7 @@ struct force {
 };
 
 struct particleNode {
-    double mass;
+    const double mass = rand() % 10000000;
     struct position position;
     struct velocity velocity;
     struct force force;
@@ -55,57 +58,37 @@ int main (int argc, char* argv[]) {
         (particleField+i)->position.x = rand() % 1000;
         (particleField+i)->position.y = rand() % 1000;
         (particleField+i)->position.z = rand() % 1000;
-        (particleField+i)->mass = rand() % 1000;
     }
     for (int i = 0; i < size; ++i) {
         auto t_initial = steady_clock::now();
         for (int j = 0; j < size; ++j) {
             if (i == j) continue;
-            auto delta_t = duration_cast<seconds>(steady_clock::now() - t_initial).count(); // change in time (t2-t1)
-            
-            
-            // double changeInX = calculateChangeInPosition((particleField+i)->position.x, (particleField+i), delta_t); // new x
-            // double changeInY = calculateChangeInPosition((particleField+i)->position.y, (particleField+i), delta_t); // new y
-            // double changeInZ = calculateChangeInPosition((particleField+i)->position.z, (particleField+i), delta_t); // new z
-            // double forceBetweenTwoParticles = calculateForce((particleField+i), (particleField+j));
-            // double acceleration = calculateAcceleration((particleField+i), forceBetweenTwoParticles);
-            // double newVelocity = calculateInstVelocity((particleField+i), delta_t, acceleration);
-            // (particleField+i)->force += forceBetweenTwoParticles; // accumulating force (total force acting on particle i)
-            // (particleField+i)->velocity = newVelocity;
-            // (particleField+i)->position.x = changeInX;
-            // (particleField+i)->position.y = changeInY;
-            // (particleField+i)->position.z = changeInZ;
+            auto delta_t = duration_cast<seconds>(steady_clock::now() - t_initial).count();
         }
     }
     return 0;
 }
 
-double calculateDistance(double coordinate1, double coordinate2) {\
+double calculateDistance(double coordinate1, double coordinate2) {
     return coordinate2-coordinate1;
 }
 
 double calculateForce(double mass1, double coordinate1, double mass2, double coordinate2) {
-    double totalMass = mass1 * mass2;
-    cout << "Total Mass: " << totalMass << endl;
-    double distance = calculateDistance(coordinate1, coordinate2);
-    cout << "Distance: " << distance << endl;
-    double distancedSquared = pow(distance, 2);
-    cout << "Distance Squared: " << distancedSquared << endl;
-    double directionOfForce = abs(distance)/distance;
-    cout << "Direction of Force: " << directionOfForce << endl;
-    double gravitationalForce = G * (totalMass/((distancedSquared + SOFTENING_FACTOR)));
-    return directionOfForce*gravitationalForce;
+    double totalMass = mass1*mass2;
+    double distanceDifference = calculateDistance(coordinate1, coordinate2);
+    double r = distanceDifference/abs(distanceDifference);
+    double rSquared = pow(r,2);
+    return r*G*(totalMass/rSquared);
 }
 
-// double calculateInstVelocity(struct particleNode* p, double delta_t, double acceleration) {
-//     return p->velocity + (acceleration*(delta_t));
-// }
+double calculatePosition(double coordinate, double velocity, double delta_t) {
+    return coordinate + velocity*delta_t;
+}
 
-// double calculateChangeInPosition(double coordinate, struct particleNode* p, double delta_t) {
-//     double velocityTimesTime = p->velocity*delta_t;
-//     return (coordinate + velocityTimesTime);
-// }
+double calculateVelocity(double velocity, double acceleration, double delta_t) {
+    return velocity + acceleration*delta_t;
+}
 
-double calculateAcceleration(struct particleNode* p, double force) {
-    return force/p->mass;
+double calculateAcceleration(double force, double mass) {
+    return force/mass;
 }
