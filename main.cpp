56f3b2b3c9dffebcb863bf8a424faabad8d5ca0a
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-09 14:29:19 -0500

    Fixed nan and -nan bugs, I tried to initialize the const double in the struct...

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono;
 
 #define ARGS 5 // num of particles, time step size, num of iterations, how often to dump state
 const double G = 6.674*pow(10,-11); // G for grav force
-const double SOFTENING_FACTOR = 0.000000001; // this is just an arbitary float (I wasn't sure what "softening factor" actuall was...)
+const double SOFTENING_FACTOR = 0.0001; // this is just an arbitary float (I wasn't sure what "softening factor" actuall was...)
 
 double calculateForceComponent(double mass1, double coordinate1, double mass2, double coordinate2);
 double calculateAcceleration(double force, double mass);
@@ -44,7 +44,7 @@ struct force {
 };
 
 struct particleNode {
-    const double mass = rand() % 10000000;
+    double mass;
     struct position position;
     struct velocity velocity;
     struct force force;
@@ -59,23 +59,30 @@ int main (int argc, char* argv[]) {
         return -1;
     }
     for (int i = 0; i < size; ++i) {
-        (particleField+i)->velocity.vx = rand() % 1000;
-        (particleField+i)->velocity.vy = rand() % 1000;
-        (particleField+i)->velocity.vz = rand() % 1000;
-        (particleField+i)->force.fx = rand() % 1000;
-        (particleField+i)->force.fy = rand() % 1000;
-        (particleField+i)->force.fz = rand() % 1000;
-        (particleField+i)->position.x = rand() % 1000;
-        (particleField+i)->position.y = rand() % 1000;
-        (particleField+i)->position.z = rand() % 1000;
+        (particleField+i)->velocity.vx = rand() % 10;
+        (particleField+i)->velocity.vy = rand() % 10;
+        (particleField+i)->velocity.vz = rand() % 10;
+        (particleField+i)->force.fx = rand() % 10;
+        (particleField+i)->force.fy = rand() % 10;
+        (particleField+i)->force.fz = rand() % 10;
+        (particleField+i)->position.x = rand() % 10;
+        (particleField+i)->position.y = rand() % 10;
+        (particleField+i)->position.z = rand() % 10;
+        (particleField+i)->mass = rand() % 10000;
     }
+    auto t_initial = steady_clock::now();
     for (int i = 0; i < size; ++i) {
-        auto t_initial = steady_clock::now();
+        cout << "Particle " << i << endl;
         for (int j = 0; j < size; ++j) {
-            if (i == j) continue;
+            if (i == j) {
+                cout << "Skipping particle " << j << endl;
+                continue;
+            }
             auto delta_t = duration_cast<seconds>(steady_clock::now() - t_initial).count();
 
             double totalForce = calculateForce((particleField+i), (particleField+j));
+            cout << "Time Step " << i << " total force on particle " << i << " from particle " << j <<  " is: " << totalForce << endl;
+
 
             double distance = calculateEuclideanDistance(
                 (particleField+i)->position.x,
@@ -85,9 +92,10 @@ int main (int argc, char* argv[]) {
                 (particleField+j)->position.y,
                 (particleField+j)->position.z
             );
+            cout << "Time Step " << i << " distance in euclidean space: " << distance << endl;
 
             (particleField+i)->force.fx = calculateForceComponent(
-                totalForce, 
+                totalForce,
                 (particleField+i)->position.x,
                 (particleField+j)->position.x,
                 distance
@@ -104,26 +112,29 @@ int main (int argc, char* argv[]) {
                 (particleField+j)->position.z,
                 distance
             );
-
+            cout << "Time Step " << i << " force component: (" << (particleField+i)->force.fx << "," << (particleField+i)->force.fy << "," << (particleField+i)->force.fz << ")" << endl;
 
             (particleField+i)->acceleration.ax = calculateAcceleration((particleField+i)->force.fx, (particleField+i)->mass);
             (particleField+i)->acceleration.ay = calculateAcceleration((particleField+i)->force.fy, (particleField+i)->mass);
             (particleField+i)->acceleration.az = calculateAcceleration((particleField+i)->force.fz, (particleField+i)->mass);
-            
+            cout << "Time Step " << i << " acceleration: (" << (particleField+i)->acceleration.ax << "," << (particleField+i)->acceleration.ay << "," << (particleField+i)->acceleration.az << ")" << endl;
+
             (particleField+i)->velocity.vx = calculateVelocity((particleField+i)->velocity.vx, (particleField+i)->acceleration.ax, delta_t);
             (particleField+i)->velocity.vy = calculateVelocity((particleField+i)->velocity.vy, (particleField+i)->acceleration.ay, delta_t);
             (particleField+i)->velocity.vz = calculateVelocity((particleField+i)->velocity.vz, (particleField+i)->acceleration.az, delta_t);
+            cout << "Time Step " << i << " velocity: (" << (particleField+i)->velocity.vx << "," << (particleField+i)->velocity.vy << "," << (particleField+i)->velocity.vz << ")" << endl;
 
             (particleField+i)->position.x = calculatePosition((particleField+i)->position.x, (particleField+i)->velocity.vx, delta_t);
             (particleField+i)->position.y = calculatePosition((particleField+i)->position.y, (particleField+i)->velocity.vy, delta_t);
             (particleField+i)->position.z = calculatePosition((particleField+i)->position.z, (particleField+i)->velocity.vz, delta_t);
+            cout << "Time Step " << i << " position: (" << (particleField+i)->position.x << "," << (particleField+i)->position.y << "," << (particleField+i)->position.z << ")" << endl;
         }
     }
     return 0;
 }
 
 double calculateEuclideanDistance(double x1, double y1, double z1, double x2, double y2, double z2) {
-    return sqrt(pow((x2-x1), 2) + pow((y2-y1), 2) + pow((z2-z1), 2));
+    return sqrt((pow((x2-x1), 2) + pow((y2-y1), 2) + pow((z2-z1), 2)));
 }
 
 double calculateLinearDistance(double coordinate1, double coordinate2) {
@@ -132,6 +143,7 @@ double calculateLinearDistance(double coordinate1, double coordinate2) {
 
 double calculateForce(struct particleNode* p1, struct particleNode* p2) {
     double totalMass = p1->mass*p2->mass;
+    cout << "Total Mass: " << totalMass << endl;
     double distance = calculateEuclideanDistance(
         p1->position.x,
         p1->position.y,
@@ -140,7 +152,8 @@ double calculateForce(struct particleNode* p1, struct particleNode* p2) {
         p2->position.y,
         p2->position.z
     );
-    return (distance/abs(distance))*G*(totalMass/(pow(distance,2)));
+    cout << "Distance: " << distance << endl;
+    return (distance/abs(distance))*G*(totalMass/(pow(distance,2)+SOFTENING_FACTOR));
 }
 
 double calculatePosition(double coordinate, double velocity, double delta_t) {
