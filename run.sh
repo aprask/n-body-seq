0812fa0ade56959944d5464813ff87fadd6255f9
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-12 21:49:36 -0500

    Fixed script

--- run.sh
+++ run.sh
@@ -6,8 +6,5 @@ if [ $? -ne 0 ]; then
     exit 1
 fi 
 ./nbod solar 200 5000000 5
-python3 python3 plot.py results.tsv solar_output.pdf
 ./nbod 100 1 10000 5
-python3 python3 plot.py results.tsv rand_output_1.pdf
 ./nbod 1000 1 10000 5
-python3 python3 plot.py results.tsv rand_output_1.pdf
