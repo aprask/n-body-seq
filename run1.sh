ff08eb7b48cfd74ed88d1064f8d82058875cd9af
Andrew Terrence Praskala <apraskal@gal-i9.charlotte.edu>
2025-02-14 14:28:03 -0500

    Adding files from slurm

--- run1.sh
+++ run1.sh
@@ -5,6 +5,4 @@ if [ $? -ne 0 ]; then
     echo "Build failed. Exiting."
     exit 1
 fi 
-./nbod solar 200 5000000 5
-./nbod 100 1 10000 5
-./nbod 1000 1 10000 5
+./nbod solar 200 5000000 1000000
--- run3.sh
+++ run3.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#/bin/bash
 make clean
 make
 if [ $? -ne 0 ]; then
     echo "Build failed. Exiting."
     exit 1
 fi 
-./nbod solar 200 5000000 5
-./nbod 100 1 10000 5
-./nbod 1000 1 10000 5
+./nbod 1000 1 10000 10000
