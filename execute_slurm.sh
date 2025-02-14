5065645fa092ad11daad632c8509bae67d9ebddf
Andrew Terrence Praskala <apraskal@gal-i9.charlotte.edu>
2025-02-14 15:22:18 -0500

    Added more files

--- execute_slurm.sh
+++ execute_slurm.sh
@@ -5,7 +5,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=128G
-./run1.sh
-./run2.sh
-./run3.sh
-./run4.sh
+./run5.sh
---
+++ run5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make clean
+make
+if [ $? -ne 0 ]; then
+    echo "Build failed. Exiting."
+    exit 1
+fi
+./nbod 1000 1 10000 5000
